73b86d232e4af9860f0b4b2bcbb513a95f398009
Allg17 <albert_lopez@ucne.edu.do>
2022-05-27 17:14:50 -0400

    Add project files.

---
+++ MonkeyFinder/Services/MonkeyService.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
+
+public class MonkeyService
+{
+    HttpClient httpClient;
+    public MonkeyService()
+    {
+        this.httpClient = new HttpClient();
+    }
+
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+        List<Monkey> monkeyList = new();
+        var response = await httpClient.GetAsync("https://www.montemagno.com/monkeys.json");
+
+        if (response.IsSuccessStatusCode)
+        {
+            monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+        }
+        return monkeyList;
+    }
+}
---
+++ MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MonkeyFinder.ViewModel;
+
+public abstract class BaseViewModel : INotifyPropertyChanged
+{
+
+    public bool IsBusy { get; set; }
+    public bool IsNotBusy => !IsBusy;
+    public string Title { get; set; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName] string name = null) =>
+    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
